3e97d88691e418aa916397eaa16b1113c89ee247
matheus-alan-inovadora <matheus.alan.souza@gmail.com>
2021-01-07 14:29:54 -0300

    Categorias do site

--- vendor/hcodebr/php_classes/src/Mailer.php
+++ vendor/hcodebr/php_classes/src/Mailer.php
@@ -7,11 +7,7 @@ use Rain\Tpl;
 class Mailer {
 
 	const USERNAME = "matheus.alan.souza.dias@gmail.com";
-<<<<<<< HEAD
     const PASSWORD = "988110401Matheus";
-=======
-    const PASSWORD = "xxxxxxxxxxxxxx";
->>>>>>> 34bef716a19fe21527478aaf71eb5feb0048adcf
     const NAME_FROM = "Loja do Matheus";
 
 	public function __construct($toAddress, $toName, $subject, $tplName, $data = array()){
